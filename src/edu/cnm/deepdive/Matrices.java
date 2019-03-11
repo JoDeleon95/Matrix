0d3449a210a4540e1558503f46f372af16f22555
“JoDeLeon95” <JoDeLeon95@gmail.com>
2019-03-11 08:04:47 -0600

    Added Junit testing and implemented rotation

--- src/edu/cnm/deepdive/Matrices.java
+++ src/edu/cnm/deepdive/Matrices.java
@@ -1,10 +1,19 @@
 package edu.cnm.deepdive;
 
+
 public class Matrices {
 
-  public static int[][] rotate(int[][] data) {
-    int [][] returnData = new int[data.length][data.length];
-    
-  }
 
+  public static int[][] rotateClock(int[][]input) {
+
+    int[][]Rotated = new int[input.length][input.length];
+
+    for (int i = 0; i < input.length; i++) {
+      for (int j = 0; j < input.length; j++) {
+        Rotated[i][j] = input[input.length - 1 - j][i];
+      }
+
+    }
+    return Rotated;
+  }
 }
